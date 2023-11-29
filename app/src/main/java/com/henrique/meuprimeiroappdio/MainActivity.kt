94e4fb27a958e2d77646001fa719bfb5ca0b78a0
Henrique de Andrade <henriquemolinos@gmail.com>
2023-11-29 20:42:04 -0300

    Commit desafio: alunos do curso Entendendo a Estrutura e Depurando Apps Android

---
+++ app/src/main/java/com/henrique/meuprimeiroappdio/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.henrique.meuprimeiroappdio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // O desafio será criar um valor dentro do string.xml e trocar o texto para tornar internacional
+    }
+}
